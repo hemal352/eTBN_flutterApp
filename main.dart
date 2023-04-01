4523b69a89a1f4307c62bcf7584995f09d7b347c
Hemal Gunasingha <73220542+hemal352@users.noreply.github.com>
2023-04-01 14:06:12 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,12 @@
+import 'package:etbn_flutter_app/app/pages/onbording_page.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(
+    const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Onbording(),
+    ),
+  );
+}
